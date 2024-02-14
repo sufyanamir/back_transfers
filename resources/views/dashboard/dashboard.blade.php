3cf80876d6e3d54a56337c4293668f8a61907b9d
MuhammadNasir1 <nmian7080@gmail.com>
2024-02-14 20:19:12 +0500

    change in dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,295 +1,143 @@
-
-
-
 @include('dashboard.layouts.header')
 
 <div class="bg-slate-50">
     <div class="p-5 flex flex-col">
         <span class="text-2xl font-semibold font-montserrat">Dashboard</span>
-        <span>Dashboard - Edit Profile</span>
-     
-     </div>
-     
-         <div class="p-4  rounded-lg">
-            <div class="grid xl:grid-cols-4 gap-4 mb-4">
-               <div class="flex items-center gap-5 justify-center  py-10 rounded-xl bg-white shadow-  dark:bg-gray-800">
-                  <div class="bg-slate-100 rounded-xl w-14 h-14 flex justify-center items-center">
-                      <img src={{asset('assets/icons/mybooking.svg')}} alt="" class="w-8">
-                  </div>
-                  <div class="flex flex-col justify-center items-center w-[132px] ">
+        <span>Dashboard - Edit rofile</span>
+
+    </div>
+
+    <div class="p-4  rounded-lg">
+        <div class="grid xl:grid-cols-4 gap-4 mb-4">
+            <div class="flex items-center gap-5 justify-center  py-10 rounded-xl bg-white shadow-dark  dark:bg-gray-800">
+                <div class="bg-slate-100 rounded-xl w-14 h-14 flex justify-center items-center">
+                    <img src={{ asset('assets/icons/mybooking.svg') }} alt="" class="w-8">
+                </div>
+                <div class="flex flex-col justify-center items-center w-[132px] ">
                     <span class="text-lg font-montserrat">My Booking</span>
                     <span class="font-bold text-4xl">93</span>
-                  </div>
-               </div>
-               <div class="flex items-center gap-5 justify-center  py-10 rounded-xl bg-white shadow-sm dark:bg-gray-800">
-                 <div class="bg-slate-100 rounded-xl w-14 h-14 flex justify-center items-center">
-                     <img src={{asset('assets/icons/invoice.svg')}} alt="" class="w-8">
-                 </div>
-                 <div class="flex flex-col justify-center items-center  w-[132px]">
-                   <span class="text-lg font-montserrat">Invoice</span>
-                   <span class="font-bold text-4xl">93</span>
-                 </div>
-              </div>
-              <div class="flex items-center gap-5 justify-center  py-10 rounded-xl bg-white shadow-sm dark:bg-gray-800">
-                 <div class="bg-slate-100 rounded-xl w-14 h-14 flex justify-center items-center">
-                     <img src={{asset('assets/icons/pendingPayment.svg')}} alt="" class="w-8">
-                 </div>
-                 <div class="flex flex-col justify-center items-center  ">
-                   <span class="text-lg font-montserrat">Pending Payment</span>
-                   <span class="font-bold text-4xl">93</span>
-                 </div>
-              </div>
-               {{--  --}}
-               <div class="flex items-center gap-5 justify-center  py-10 rounded-xl bg-white shadow-sm dark:bg-gray-800">
-                 <div class="bg-slate-100 rounded-xl w-14 h-14 flex justify-center items-center">
-                     <img src={{asset('assets/icons/location.svg')}} alt="" class="w-8">
-                 </div>
-                 <div class="flex flex-col justify-center items-center ">
-                   <span class="text-lg font-montserrat">Saved Loacation</span>
-                   <span class="font-bold text-4xl">93</span>
-                 </div>
-              </div>
+                </div>
             </div>
+            <div class="flex items-center gap-5 justify-center  py-10 rounded-xl bg-white shadow-dark dark:bg-gray-800">
+                <div class="bg-slate-100 rounded-xl w-14 h-14 flex justify-center items-center">
+                    <img src={{ asset('assets/icons/invoice.svg') }} alt="" class="w-8">
+                </div>
+                <div class="flex flex-col justify-center items-center  w-[132px]">
+                    <span class="text-lg font-montserrat">Invoice</span>
+                    <span class="font-bold text-4xl">93</span>
+                </div>
+            </div>
+            <div class="flex items-center gap-5 justify-center  py-10 rounded-xl bg-white shadow-dark dark:bg-gray-800">
+                <div class="bg-slate-100 rounded-xl w-14 h-14 flex justify-center items-center">
+                    <img src={{ asset('assets/icons/pendingPayment.svg') }} alt="" class="w-8">
+                </div>
+                <div class="flex flex-col justify-center items-center  ">
+                    <span class="text-lg font-montserrat">Pending Payment</span>
+                    <span class="font-bold text-4xl">93</span>
+                </div>
+            </div>
+            {{--  --}}
+            <div class="flex items-center gap-5 justify-center  py-10 rounded-xl bg-white shadow-dark   dark:bg-gray-800">
+                <div class="bg-slate-100 rounded-xl w-14 h-14 flex justify-center items-center">
+                    <img src={{ asset('assets/icons/location.svg') }} alt="" class="w-8">
+                </div>
+                <div class="flex flex-col justify-center items-center ">
+                    <span class="text-lg font-montserrat">Saved Loacation</span>
+                    <span class="font-bold text-4xl">93</span>
+                </div>
+            </div>
+        </div>
         {{-- data Table --}}
-        <div class="container w-full md:w-full xl:w-full mx-auto px-2">
-     
-         <!--Title-->
-         {{-- <h1 class="flex items-center font-sans font-bold break-normal text-indigo-500 px-2 py-8 text-xl md:text-2xl">
-             Responsive <a class="underline mx-2" href="https://datatables.net/">DataTables.net</a> Table
-         </h1> --}}
-     
-     
-         <!--Card-->
-         <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white ">
-     
-     
-             <table id="dataTable" class="display" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
-                 <thead>
-                     <tr>
-                         <th data-priority="1">Name</th>
-                         <th data-priority="2">Position</th>
-                         <th data-priority="3">Office</th>
-                         <th data-priority="4">Age</th>
-                         <th data-priority="5">Start date</th>
-                         <th data-priority="6">Salary</th>
-                     </tr>
-                 </thead>
-                 <tbody>
-                     <tr class="bg-white">
-                         <td>Tiger Nixon</td>
-                         <td>System Architect</td>
-                         <td>Edinburgh</td>
-                         <td>61</td>
-                         <td>2011/04/25</td>
-                         <td>$320,800</td>
-                     </tr>
-     
-                     <!-- Rest of your data (refer to https://datatables.net/examples/server_side/ for server side processing)-->
-     
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     {{--  --}}
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                     <tr>
-                         <td>Donna Snider</td>
-                         <td>Customer Support</td>
-                         <td>New York</td>
-                         <td>27</td>
-                         <td>2011/01/25</td>
-                         <td>$112,000</td>
-                     </tr>
-                 </tbody>
-     
-             </table>
-     
-     
-         </div>
-         <!--/Card-->
-     
-     
-     </div>
-     <!--/container-->
-     
-     
+        <div class="mt-8 ">
+
+            <div class="shadow-dark mt-3  rounded-xl pt-8  bg-white min-h-[500px]">
+                <div>
+                    <div class="flex justify-between px-[20px] mb-3">
+                        <h3 class="text-[20px] text-black">Current Booking</h3>
+                        <button data-modal-target="addteachermodal" data-modal-toggle="addteachermodal"
+                            class="bg-secondary text-white h-12 px-5 rounded-[6px]  shadow-sm font-semibold ">sdlkf)</button>
+                    </div>
+                    <table id="datatable" class="overflow-scroll">
+                        <thead class="py-6 text-black">
+                            <tr>
+                                <th>Order ID</th>
+                                <th>Date - Time</th>
+                                <th>From - To</th>
+                                <th>Total</th>
+                                <th>Payment Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="pt-4">
+                                <td>#953</td>
+                                <td>Thu, Oct 06, 2022 - 6 PM : 15</td>
+                                <td>London City Airport (LCY) - New York City Airport</td>
+                                <td>$2,865.03</td>
+                                <td>
+                                    <button class="bg-[#F7F3FF] text-[#8442FF] py-2 px-4">Pending</button>
+                                </td>
+
+
+                                <td class="flex gap-5">
+                                    <a class="cursor-pointer" href="#"><img width="38px"
+                                            src="{{ asset('assets/icons/delete.svg') }}" alt="delete"></a>
+                                    <a class="cursor-pointer" href="#"><img width="38px"
+                                            src="{{ asset('assets/icons/update.svg') }}" alt="update"></a>
+                                </td>
+                            </tr>
+                            <tr class="pt-4">
+                                <td>#953</td>
+                                <td>Thu, Oct 06, 2022 - 6 PM : 15</td>
+                                <td>London City Airport (LCY) - New York City Airport</td>
+                                <td>$2,865.03</td>
+                                <td>
+                                    <button class="bg-[#F7F3FF] text-[#8442FF] py-2 px-4">Pending</button>
+                                </td>
+
+
+                                <td class="flex gap-5">
+                                    <a class="cursor-pointer" href="#"><img width="38px"
+                                            src="{{ asset('assets/icons/delete.svg') }}" alt="delete"></a>
+                                    <a class="cursor-pointer" href="#"><img width="38px"
+                                            src="{{ asset('assets/icons/update.svg') }}" alt="update"></a>
+                                </td>
+                            </tr>
+                            <tr class="pt-4">
+                                <td>#953</td>
+                                <td>Thu, Oct 06, 2022 - 6 PM : 15</td>
+                                <td>London City Airport (LCY) - New York City Airport</td>
+                                <td>$2,865.03</td>
+                                <td>
+                                    <button class="bg-[#F7F3FF] text-[#8442FF] py-2 px-4">Pending</button>
+                                </td>
+
+
+                                <td class="flex gap-5">
+                                    <a class="cursor-pointer" href="#"><img width="38px"
+                                            src="{{ asset('assets/icons/delete.svg') }}" alt="delete"></a>
+                                    <a class="cursor-pointer" href="#"><img width="38px"
+                                            src="{{ asset('assets/icons/update.svg') }}" alt="update"></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+            <!--/Card-->
+
+
+        </div>
+        <!--/container-->
+
+
         {{-- data Table --}}
-     
-     
-     
-     
-         </div>
+
+
+
+
+    </div>
 
 
 
--- resources/views/dashboard/layouts/footer.blade.php
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -1,5 +1,4 @@
-
-  <hr class="border border-gray-300">
+<hr class="border border-gray-300">
 <div class="  p-5 flex flex-wrap justify-between items-center ">
 
     <div>
@@ -16,9 +15,18 @@
 
 </div>
 </div>
-<script src="{{asset('assets/js/jquery.min.js')}}"></script>
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-<script src="{{asset('assets/js/dataTable.min.js')}}"></script>
-<script src="{{asset('assets/js/custom.js')}}"></script>
+</div>
+
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"
+    integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="{{ asset('assets/js/dataTable.min.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        $('#datatable').DataTable();
+    });
+</script>
 </body>
+
 </html>
